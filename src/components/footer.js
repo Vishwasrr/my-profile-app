03aeac361c1d336241223bfdae09a33d4591abe2
Vishwas R <48607495+Vishwasrr@users.noreply.github.com>
2022-08-22 20:46:23 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,11 +105,8 @@ const Footer = () => {
           href="https://github.com/bchiang7/v4"
           target="_blank"
           rel="nofollow noopener noreferrer">
-          <div>
-            Designed &amp; Built by Brittany Chiang<br></br>
-            Revised by Vishwas. R
+          <div>*******
           </div>
-
           {githubInfo.stars && githubInfo.forks && (
             <StyledGitHubInfo>
               <span>
